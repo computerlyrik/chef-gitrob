b9b7ccf670b62fd2e16cd37519687c17760723c2
Christian Fischer <sw-dev@computerlyrik.de>
2015-01-16 02:52:35 +0100

    berks init

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'gitrob'
+maintainer       'computerlyrik, Christian Fischer'
+maintainer_email 'chef-cookbooks@computerlyrik.de'
+license          'Apache 2.0'
+description      'Installs/Configures gitrob'
+long_description 'Installs/Configures gitrob'
+version          '0.1.0'
